37d7fe0043d665ade87c7f6c6f1e6bb3518d0c88
sommeow <s.pimson@gmail.com>
2019-07-12 15:39:07 -0500

    rendering of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -7,7 +7,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
         <main>
-          YOUR TWO COMPONENTS WILL GO HERE
+          <section className="CircleSelector">
+            <button>Select Circle 1</button>
+            <button>Select Circle 2</button>
+            <button>Select Circle 3</button>
+            <button>Select Circle 4</button>
+          </section>
+          <section className="Circles">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+          </section>
         </main>
       </div>
     );
